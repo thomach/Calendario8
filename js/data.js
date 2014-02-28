ff9b225e9d4a17fe9ff8aeff928904ec117aae70
Chris Thomas <christopher.thomas@cbac.co.uk>
2014-02-28 08:23:51 +0000

    Calendario 8
    This version has a click for answer button but I'm having difficulty in
    having the day event open or the question open on the day to be open on
    the day?

---
+++ js/data.js
@@ -0,0 +1,49 @@
+var codropsEvents = {
+
+	
+'MM-01-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-02-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-03-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-04-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-05-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-06-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-07-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-08-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-09-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-10-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-11-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-12-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-13-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-14-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-15-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-16-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-17-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-18-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-19-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-20-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-21-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-22-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-23-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-24-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-25-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-26-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-27-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-28-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-29-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-30-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+'MM-31-YYYY' : '<a href="#"><span>Point-gradient formula</span><div class="question">Question goes here!</div><div class="answer">Answer will go here.</div></a>',
+
+
+
+
+	'02-23-YYYY' : '<a href="http://tympanus.net/codrops/2012/11/23/three-script-updates/">Three Script Updates</a>',
+
+	'02-24-YYYY' : '<a href="http://tympanus.net/codrops/2012/11/23/three-script-updates/">Three Script Updates</a>',
+	
+
+
+	'02-21-2014' : '<a href="http://tympanus.net/codrops/2012/11/21/adaptive-thumbnail-pile-effect-with-automatic-grouping/">Adaptive Thumbnail Pile Effect with Automatic Grouping</a>',
+
+	'12-25-YYYY' : '<a href="#"><span>Christmas Day</span></a>',
+	'12-31-YYYY' : '<a href="#"><span>New Year\'s Eve</span></a>'
+};
